9ea30bb305f5acb1ac0115104c56475efd360ea0
kaave <kyousuke.abe@aliinc.co.jp>
2016-03-29 19:09:25 +0900

    Update template, add key, publish

--- dist/index.js
+++ dist/index.js
@@ -16,21 +16,28 @@ var args = argv.option([{
   description: 'create date(YYYY-MM-DD or YYYY/MM/DD), default=today',
   example: "'--date=2016-09-24' or '-d 2016/09/24'"
 }, {
+  name: 'key',
+  short: 'k',
+  type: 'string',
+  description: 'create title(my-birth-day), default=ordinary-day',
+  example: "'--key=my-birth-day' or '-k my-birth-day'"
+}, {
   name: 'title',
   short: 't',
   type: 'string',
-  description: 'create title(my-birth-day), default=ordinary-day',
-  example: "'--title=my-birth-day' or '-t 2016/09/24'"
+  description: 'create key(私の誕生日), default=こんにちわ',
+  example: "'--title=私の誕生日' or '-t 私の誕生日'"
 }]).run().options;
 
 var date = args.date && args.date.match(/^\d{4}[-/]\d{1,2}[-/]\d{1,2}$/) ? moment(args.date) : moment();
-var title = args.title || 'ordinaly-day';
+var key = args.key || 'ordinary-day';
+var title = args.title || 'こんにちわ';
 var template = function (date, title) {
-  return ('\n---\ndate: ' + date + '\ntitle: ' + title + '\ntags: []\n---\n\n# ' + title + '\n').trimLeft();
+  return ('\n---\ndate: ' + date + '\ntitle: ' + title + '\ntags: []\npublish: false\n---\n\n## ' + title + '\n### 仕事\n### めし\n### 買い物\n### お勉強\n### 遊び\n').trimLeft();
 }(date.format('YYYY-MM-DD'), title);
 
 var dirPath = baseDir + date.format('YYYY/MM') + '/';
-var fileName = date.format('DD') + '-' + title + '.md';
+var fileName = date.format('DD') + '-' + key + '.md';
 var filePath = dirPath + fileName;
 
 // add directory
@@ -39,7 +46,7 @@ fs.mkdirsSync(appRootPath + dirPath);
 // check file exists
 var existsFile = glob.GlobSync(appRootPath + dirPath + date.format('DD') + '-*.md').found;
 if (existsFile.length > 0) {
-  console.log('Already created...: ' + existsFile.join(', '));
+  console.error('Already created...: ' + existsFile.join(', '));
 } else {
   // write file
   fs.writeFileSync(filePath, template);
--- src/index.js
+++ src/index.js
@@ -16,29 +16,43 @@ const args = argv.option([
     example: "'--date=2016-09-24' or '-d 2016/09/24'"
   },
   {
+    name: 'key',
+    short: 'k',
+    type: 'string',
+    description: 'create title(my-birth-day), default=ordinary-day',
+    example: "'--key=my-birth-day' or '-k my-birth-day'"
+  },
+  {
     name: 'title',
     short: 't',
     type: 'string',
-    description: 'create title(my-birth-day), default=ordinary-day',
-    example: "'--title=my-birth-day' or '-t 2016/09/24'"
+    description: 'create key(私の誕生日), default=こんにちわ',
+    example: "'--title=私の誕生日' or '-t 私の誕生日'"
   }
 ]).run().options;
 
 const date = args.date && args.date.match(/^\d{4}[-/]\d{1,2}[-/]\d{1,2}$/) ? moment(args.date) : moment();
-const title = args.title || 'ordinaly-day';
+const key = args.key || 'ordinary-day';
+const title = args.title || 'こんにちわ';
 const template = ((date, title) => `
 ---
 date: ${date}
 title: ${title}
 tags: []
+publish: false
 ---
 
-# ${title}
+## ${title}
+### 仕事
+### めし
+### 買い物
+### お勉強
+### 遊び
 `.trimLeft()
 )(date.format('YYYY-MM-DD'), title);
 
 const dirPath = `${baseDir + date.format('YYYY/MM')}/`;
-const fileName = `${date.format('DD')}-${title}.md`;
+const fileName = `${date.format('DD')}-${key}.md`;
 const filePath = dirPath + fileName;
 
 // add directory
@@ -47,7 +61,7 @@ fs.mkdirsSync(appRootPath + dirPath);
 // check file exists
 const existsFile = glob.GlobSync(`${appRootPath + dirPath + date.format('DD')}-*.md`).found;
 if (existsFile.length > 0) {
-  console.log(`Already created...: ${existsFile.join(', ')}`);
+  console.error(`Already created...: ${existsFile.join(', ')}`);
 } else {
   // write file
   fs.writeFileSync(filePath, template);
